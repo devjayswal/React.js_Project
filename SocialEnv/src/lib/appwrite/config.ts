a6a4570f8dd7f43af747debab4981fe0169459d2
devjayswal <rdssjayswal@gmail.com>
2023-11-14 23:23:13 +0530

    My First Full-fleged React App

--- SocialEnv/src/lib/appwrite/config.ts
+++ SocialEnv/src/lib/appwrite/config.ts
@@ -5,7 +5,7 @@ import { Import } from 'lucide-react';
 export const appwriteConfig = {
     url: import.meta.env.VITE_APPWRITE_URL,
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASES_ID,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
     storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
     userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
     postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
